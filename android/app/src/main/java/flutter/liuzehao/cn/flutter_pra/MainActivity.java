ca8d9d0b3e96e5ee71ae599e51f924f988b489fb
刘泽浩 <15088099441@163.com>
2019-10-25 17:59:03 +0800

    Initial commit

---
+++ android/app/src/main/java/flutter/liuzehao/cn/flutter_pra/MainActivity.java
@@ -0,0 +1,13 @@
+package flutter.liuzehao.cn.flutter_pra;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
